bc82b7ccaf6c1e15c08566a94ff625d1dadaecc0
Marco <mclarenmarco998@gmail.com>
2023-01-16 18:49:07 +0200

    maasdf

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,8 +5,10 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTO;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -16,12 +18,21 @@ namespace API.Controllers
             _context = context; 
         }
 
+        //To Check if username already exists
+        private async Task<bool> UserExists(string username){
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password){
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto){
+
+            if(await UserExists(registerDto.UserName)) return BadRequest("Username is taken");
+
             using var hmac = new HMACSHA512();
+
             var user = new AppUser{
-                UserName = username,
-                PassWordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.UserName.ToLower(),
+                PassWordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PassWordSalt = hmac.Key
             };
 
@@ -30,5 +41,25 @@ namespace API.Controllers
 
             return user;
         }
+
+    [HttpPost("login")]//root parameter of LOGIN 
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){ //call it Login,     //Public async task that will return an AppUser
+    var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName ==loginDto.UserName);
+
+    if(user == null) return Unauthorized("Invalid Username");
+
+     using var hmac = new HMACSHA512(user.PassWordSalt);
+
+     var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+     for(int i = 0; i< computedHash.Length; i++){
+        if(computedHash[i] != user.PassWordHash[i]) return Unauthorized("Invalid Password");
+     }
+
+     return user;
+
     }
+    
+
+}
 }
